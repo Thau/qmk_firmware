e32da4264c35b705f4f06e745af5b882b59ad580
Gonzalo Quero <me@gonzaloquero.com>
2020-03-16 17:29:32 +0000

    Add media keys with tap dance

--- keyboards/massdrop/ctrl/keymaps/thau/keymap.c
+++ keyboards/massdrop/ctrl/keymaps/thau/keymap.c
@@ -12,14 +12,28 @@ enum ctrl_keycodes {
 
 keymap_config_t keymap_config;
 
+enum {
+    TD_F9 = 0,
+    TD_F10 = 1,
+    TD_F11 = 2,
+    TD_F12 = 3,
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_F9] = ACTION_TAP_DANCE_DOUBLE(KC_F9, KC_MEDIA_PREV_TRACK),
+    [TD_F10] = ACTION_TAP_DANCE_DOUBLE(KC_F10, KC_MEDIA_STOP),
+    [TD_F11] = ACTION_TAP_DANCE_DOUBLE(KC_F11, KC_MEDIA_PLAY_PAUSE),
+    [TD_F12] = ACTION_TAP_DANCE_DOUBLE(KC_F12, KC_MEDIA_NEXT_TRACK)
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,             KC_PSCR, TO(2), KC_PAUS, \
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,   KC_INS,  KC_HOME, KC_PGUP, \
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,   KC_DEL,  KC_END,  KC_PGDN, \
-        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT, \
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                              KC_UP, \
-        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MO(1),   KC_APP,  KC_RCTL,            KC_LEFT, KC_DOWN, KC_RGHT \
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   TD(TD_F9), TD(TD_F10),  TD(TD_F11),  TD(TD_F12),         KC_PSCR, TO(2), KC_PAUS, \
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,      KC_0,        KC_MINS,     KC_EQL,  KC_BSPC,   KC_INS,  KC_HOME, KC_PGUP, \
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,      KC_P,        KC_LBRC,     KC_RBRC, KC_BSLS,   KC_DEL,  KC_END,  KC_PGDN, \
+        KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,      KC_SCLN,     KC_QUOT,     KC_ENT, \
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,    KC_SLSH,     KC_RSFT,                                  KC_UP, \
+        KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT,   MO(1),       KC_APP,      KC_RCTL,            KC_LEFT, KC_DOWN, KC_RGHT \
     ),
     [1] = LAYOUT(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            KC_MUTE, _______, _______, \
@@ -59,10 +73,11 @@ struct color {
 #define RED   { 0xFF, 0x00, 0x00 }
 #define BLUE  { 0x00, 0xFF, 0x00 }
 #define GREEN { 0x00, 0x00, 0xFF }
+#define MEDIA { 0xD9, 0x5B, 0x43 }
 
 const struct color ledmaps[][MATRIX_ROWS * MATRIX_COLS] = {
     [0] = {
-        WHITE,  WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,  WHITE,  WHITE,             WHITE, BLUE,  WHITE, \
+        WHITE,  WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   MEDIA,   MEDIA,  MEDIA,  MEDIA,             WHITE, BLUE,  WHITE, \
         WHITE,  WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,  WHITE,  WHITE,  WHITE,     WHITE, WHITE, WHITE, \
         WHITE,  WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,  WHITE,  WHITE,  WHITE,     WHITE, WHITE, WHITE, \
         RED,    WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,  WHITE,  WHITE, \
@@ -70,7 +85,7 @@ const struct color ledmaps[][MATRIX_ROWS * MATRIX_COLS] = {
         WHITE,  WHITE,   WHITE,                     WHITE,                              WHITE,   WHITE,  WHITE,  WHITE,             WHITE, WHITE, WHITE \
     },
     [1] = {
-        WHITE,  WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,  WHITE,  WHITE,             WHITE, BLUE,  WHITE, \
+        WHITE,  WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   MEDIA,   MEDIA,  MEDIA,  MEDIA,             WHITE, BLUE,  WHITE, \
         WHITE,  WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,  WHITE,  WHITE,  WHITE,     WHITE, WHITE, WHITE, \
         WHITE,  WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,  WHITE,  WHITE,  WHITE,     WHITE, WHITE, WHITE, \
         RED,    WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,  WHITE,  WHITE, \
@@ -78,7 +93,7 @@ const struct color ledmaps[][MATRIX_ROWS * MATRIX_COLS] = {
         WHITE,  WHITE,   WHITE,                     WHITE,                              WHITE,   WHITE,  WHITE,  WHITE,             WHITE, WHITE, WHITE \
     },
     [2] = {
-        GREEN,  WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,  WHITE,  WHITE,             BLUE,  BLUE,  BLUE, \
+        GREEN,  WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   MEDIA,   MEDIA,  MEDIA,  MEDIA,             BLUE,  BLUE,  BLUE, \
         WHITE,  WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,  WHITE,  WHITE,  WHITE,     BLUE,  BLUE,  BLUE, \
         WHITE,  WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,  WHITE,  WHITE,  WHITE,     BLUE,  BLUE,  BLUE, \
         RED,    WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,   WHITE,  WHITE,  WHITE, \
